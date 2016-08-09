1542acc43ea772862efbe463bf2ea8e348ba0917
jason kim <jasonkim@jasons-MacBook-Air-2.local>
2016-08-09 16:23:58 -0700

    Updates starter code

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,10 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardTwo === cardFour){ 
+  alert("You have a match");
+} else { 
+  alert("Sorry try again");
+}
