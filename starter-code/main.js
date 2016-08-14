33c2b3a66ed211799d2118b4af48ecf4f355cfbd
jasonkim <jckkim@yahoo.com>
2016-08-13 23:42:09 -0700

    memory game through chapter 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -8,3 +8,18 @@ if (cardTwo === cardFour){
 } else { 
   alert("Sorry try again");
 }
+
+var board = document.getElementById("#game-board");
+
+function createBoard() {
+
+	for (var i=0; i<cards.length; i++) {
+
+	var cardElement = document.createElement('div');
+
+	cardElement.className = 'card';
+
+	board.appendChild(cardElement);
+  }
+
+}
